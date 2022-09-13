2bd66dfb513b18851e120e3e4f88475981c77a8e
tqkoh <shktmygs@gmail.com>
2022-09-13 11:10:37 +0900

    add post_fetch_problems

--- src/main.rs
+++ src/main.rs
@@ -80,6 +80,7 @@ async fn main() -> std::io::Result<()> {
             .service(routes::get_submissions_sid_handler)
             .service(routes::get_submissions_handler)
             .service(routes::get_tasks_tid_handler)
+            .service(routes::post_fetch_problems_handler)
     })
     .bind((address, 8080))?
     .run()
